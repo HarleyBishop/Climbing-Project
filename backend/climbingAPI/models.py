ebff5511d43eab825f563f0bac8d5a8fba4481ba
HarleyBishop <harleyjett@hotmail.com>
2026-03-30 21:21:28 +1000

    not much done ngl keep up UML and wireframe design

--- backend/climbingAPI/models.py
+++ backend/climbingAPI/models.py
@@ -3,10 +3,18 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Climb(models.Model):
-    colour = models.CharField(max_length=100)
-    wall = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
+class Gym(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    Is_Active = models.BooleanField()
 
     def __str__(self):
-        return self.colour
+        return self.name
+    
+class Wall(models.Models):
+    name = models.CharField(max_length=100)
+    gym = models.ForeignKey('Gym', on_delete=models.CASCADE, related_name='walls')
+
+    def __str__(self):
+        return self.name
+    
